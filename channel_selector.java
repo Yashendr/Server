e075d31c59f78785a476184c808e221ebb9cf3b6
Alfred-Rademan <106009679+Alfred-Rademan@users.noreply.github.com>
2022-07-22 17:00:22 +0200

    Add files via upload

--- channel_selector.java
+++ channel_selector.java
@@ -7,8 +7,13 @@ import java.nio.channels.*;
 public class channel_selector implements Runnable {
     private boolean active = true;
     private Selector selector;
-    public channel_selector() throws IOException{
-        selector = Selector.open();
+    private SelectionKey server_key;
+    private ServerSocketChannel server_channel;
+    public channel_selector(Selector selector, SelectionKey server_key,
+    ServerSocketChannel server_channel) throws IOException {
+        this.selector = selector;
+        this.server_key = server_key;
+        this.server_channel = server_channel;
     }
     
     @Override
@@ -18,24 +23,32 @@ public class channel_selector implements Runnable {
 
             try {
                 ready = selector.selectNow();
-            } catch (Exception e) {
+
+                if(ready == 0) continue;
+
+                Set<SelectionKey> keys = selector.selectedKeys();;
+                Iterator<SelectionKey> key_checker =  keys.iterator();
+                            
+                while(key_checker.hasNext()) {
+                    SelectionKey key = key_checker.next();
+    
+                    if(!key.isValid()) continue;
+                    
+                    if(key == server_key){
+                        SocketChannel new_connection = server_channel.accept();
+                        new_connection.configureBlocking(false);
+                        int jobs = SelectionKey.OP_READ | SelectionKey.OP_WRITE;
+                        SelectionKey new_key = new_connection.register(selector, jobs);
+                    }
+    
+                    key_checker.remove();
+                }
+
+            } catch (Exception IOException) {
             
             }
 
-            if(ready == 0) continue;
-
-            Set<SelectionKey> keys = selector.selectedKeys();;
-            Iterator<SelectionKey> key_checker =  keys.iterator();
-                        
-            while(key_checker.hasNext()){
-                SelectionKey key = key_checker.next();
-
-                if(key.isReadable()){
-
-                } else if(key.isWritable()){
-
-                }
-            }
+           
 
         }
         
