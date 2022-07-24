fd6a04031cc6f1ef74596ba0d1111a0aefe9d1a7
Yashendr <72069056+Yashendr@users.noreply.github.com>
2022-07-25 00:39:58 +0200

    Add files via upload

--- channel_selector.java
+++ channel_selector.java
@@ -19,10 +19,12 @@ public class channel_selector implements Runnable {
         this.server_key = server_key;
         this.server_channel = server_channel;
         this.lock = lock;
+        this.clients = clients;
     }
     
     @Override
     public void run() {
+        String mes_test = "";
         while (active) {
             int ready = 0;
             SelectionKey key = null;
@@ -52,43 +54,78 @@ public class channel_selector implements Runnable {
                             new_connection.configureBlocking(false);
                             int jobs = SelectionKey.OP_READ | SelectionKey.OP_WRITE;
                             SelectionKey new_key = new_connection.register(selector, jobs);
-                            String username = Send_Rec.receive(new_connection);
-                            System.out.println(new_key + " " + server_key);
+                            Thread.sleep(10);
+                            String username = Send_Rec.receive(new_connection,1).trim();
+                            
                             clients.add(username, new_key);
                         }
                     }
 
                     else if(key.isReadable() && key != server_key) {
                       String message = Send_Rec.receive((SocketChannel)key.channel());
-
                       if(message.equals("")){
                         key_checker.remove();
 
                         continue;
                       }
+                      if (message.contains("!!Disconnect")){
+                        String user = message.substring(13).trim();
+                        clients.remove(user);
+                        if (key != null){
+                            key.cancel();
+                        }
+                        key_checker.remove();
+                        continue;
+                      }
+                      if (message.trim().charAt(0) == '$')  {
+                            String user = "";
+                            int i = 1; 
+                            String Send_user = message.substring(1,message.indexOf('@')).trim();
+
+                            try {
+                                if(!clients.key(Send_user).equals(key)) {
+                                    key_checker.remove();
+                                    continue;
+                                }
+                            } catch (Exception e) {
+                                key_checker.remove();
+                                continue;
+                            }
+     
+
+
+                            SocketChannel rec_chan = null;
+                            for ( i =message.indexOf('@')+1 ;message.charAt(i) != ' ';i++){
+                                    user += message.charAt(i);
+                            }
+                            rec_chan = (SocketChannel)clients.key(user).channel();
+                           
+                                if(!rec_chan.equals(null) && !Send_user.equals(user)) {
+                                    Send_Rec.send("Private message "+ Send_user +": "+message.substring(i), (rec_chan));
+                                }
+                      } else {
                       //System.out.println("GET");
                      // Set<SelectionKey> keyall = (selector.keys());
-                      Iterator<SelectionKey> allkey_checker =  keyall.iterator();
-                      
-                      while(allkey_checker.hasNext()) {
-                        SelectionKey my_key = allkey_checker.next();
-                       
-                        if (!my_key.equals(key) && my_key != server_key){
-                            Send_Rec.send(message,(SocketChannel)my_key.channel());
-        
-                        }else {
-                            System.out.println("here");
-                        }
+                        Iterator<SelectionKey> allkey_checker =  keyall.iterator();
+                        
+                        while(allkey_checker.hasNext()) {
+                            SelectionKey my_key = allkey_checker.next();
+                        
+                            if (!my_key.equals(key) && my_key != server_key){
+                                Send_Rec.send(message,(SocketChannel)my_key.channel());
+            
+                            }else {
+                                System.out.println("here");
+                            }
                       }
-                     
+                  }
                     } 
     
                     key_checker.remove();
                 }
 
-            } catch (Exception IOException) {
-                System.out.println(key.equals(server_key));
-
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
 
